40d6a89ed239c33f0d3b4d122f32a2a0ef98bef8
PUESTO_01\Fabri <evelynat21@gmail.com>
2023-06-30 22:41:31 -0300

    Detalles css

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,12 +1,12 @@
-
 import { Link } from 'react-router-dom';
 import style from './Card.module.css'
+import { Tooltip as ReactTooltip } from 'react-tooltip'
 
 const Card = (props)=> {
 
    const favs = JSON.parse(localStorage.getItem('odontologosFavs'));
    const isFavorite = favs.some((fav) => fav.id === props.user.id);
-
+   const tooltipText = isFavorite ? 'Quitar de favoritos' : 'Agregar a favoritos';
 
     return (
          <div className={style.card}>
@@ -18,11 +18,17 @@ const Card = (props)=> {
                </div>
             </div>
             <div className={style.cardFooter}>
-               <a onClick={()=> props.onClick(props.user)}>
+               <a onClick={()=> props.onClick(props.user)} data-tooltip-content={tooltipText} data-tooltip-id="heartTooltip">
                   {isFavorite ? <i className={style.heartSolid +" fa-solid fa-heart"}></i>:<i className="fa-light fa-heart"></i>}
                </a>
-               <Link to={`/dentist/${props.user.id}`}><i className="fa-light fa-circle-info"></i></Link>
+               <Link  data-tooltip-content="Ver detalles" data-tooltip-id="favTooltip" to={`/dentist/${props.user.id}`}><i className="fa-light fa-circle-info"></i></Link>
             </div>
+            <ReactTooltip id="heartTooltip" place="top" effect="solid">
+               {tooltipText}
+            </ReactTooltip>
+            <ReactTooltip id="favTooltip" place="top" effect="solid">
+               Ver detalles
+            </ReactTooltip>
          </div>
          );
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Card from "../components/card/Card";
+import 'react-tooltip/dist/react-tooltip.css';
 
 function Home() {
 
